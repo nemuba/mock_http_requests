fdb432fe1974a795046692117208b2e70260210c
Alef Ojeda de Oliveira <alef.oliveira@dimensa.com.br>
2023-02-20 19:06:35 -0300

    added concern handle_exception

---
+++ app/controllers/concerns/handle_exception.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# HandleException
+module HandleException
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from StandardError, with: :render_error
+  end
+
+  def render_error
+    json_response({ message: 'Internal Server Error' }, :internal_server_error)
+  end
+end
